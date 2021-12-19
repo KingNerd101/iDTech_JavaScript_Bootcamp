097c3fa91a728a25e4d9b27756e6a01ba7cc66a8
Ayden <aydennakamura909@gmail.com>
2021-12-18 22:12:47 -0800

    twoset

---
+++ Unit Testing Activity/UnitTestingAyden.js
@@ -0,0 +1,43 @@
+function GreetAyden(name) {
+    let returnvalue;
+
+    if (name == null) {
+        let UWU = ["H", "I"];
+        if (typeof(name) == typeof(UWU)) {
+            
+            returnvalue = "Hello ";
+            for (let x = 0; x < length.name; x++) {
+                returnvalue += name[x];
+            }
+            returnvalue += "!";
+        } else {
+            returnvalue = "Hello " + name;
+        }
+    } else {
+        returnvalue = "Hello there!";
+    }
+    
+
+
+
+
+
+
+    if (isUpperCase(name)) {
+        let finaltesst = returnvalue.toUpperCase;
+        // returnvalue = returnvalue.toUpperCase;
+        return finaltesst;
+    }
+}
+
+
+function isUpperCase(stringTest) {
+    return stringTest === stringTest.toUpperCase();
+}
+
+
+
+
+
+let UWUChan = ["Jimmy", "John", "Peppa"];
+GreetAyden(UWUChan);
